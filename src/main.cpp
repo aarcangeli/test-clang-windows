6a3beb58ebf55df426909cd8a8477456c71ea178
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2022-04-28 17:23:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <vector>
 
-std::atomic_bool x = false;
+std::atomic_bool x;
 
 void print() {
     std::cout << "CIAO\n";
@@ -10,5 +10,6 @@ void print() {
 
 int main() {
     std::vector<int> a;
+    x = false;
     std::cout << x;
 }
